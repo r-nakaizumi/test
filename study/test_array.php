bee93c9c21a09391de0fdfeb0ca4c739ff883a43
r-nakaizumi <r-nakaizumi@crm-coltd.com>
2018-09-14 13:01:43 +0900

    array_test

---
+++ study/test_array.php
@@ -0,0 +1,17 @@
+
+<?php
+
+$foods = array("そば","ラーメン","うどん");
+
+echo $foods[0]."<br/>";
+echo $foods[1]."<br/>";
+echo $foods[2]."<br/>";
+
+$fruits[] = "リンゴ";
+$fruits[] = "バナナ";
+$fruits[] = "オレンジ";
+
+echo $fruits[0]."<br/>";
+echo $fruits[1]."<br/>";
+echo $fruits[2]."<br/>";
+

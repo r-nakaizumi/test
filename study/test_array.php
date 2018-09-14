7ea51b0d5bc7616d3bddb14d1febbe3daf14c761
r-nakaizumi <r-nakaizumi@crm-coltd.com>
2018-09-14 13:51:25 +0900

    test_study

--- study/test_array.php
+++ study/test_array.php
@@ -20,8 +20,36 @@ $var2 = 100;
 $var3 = TRUE;
 $array = array("日本","アメリカ","イギリス");
 
-var_dump($var1); echo "<br/>";
-var_dump($var2); echo "<br/>";
-var_dump($var3); echo "<br/>";
-var_dump($array); echo "<br/>";
+//var_dump($var1); echo "<br/>";
+//var_dump($var2); echo "<br/>";
+//var_dump($var3); echo "<br/>";
+//var_dump($array); echo "<br/>";
+
+$stringData = "文字列";
+$integerData = 10;
+$floatData = 0.15;
+$boolData = TRUE;
+$arrayData = array(1,2,3);
+
+class test{}
+$objectData = new test();
+
+$resourceData = fopen("1-1-1-48-txt.php","r");
+$nullData = null;
+$allData = array();
+
+$allData = array(
+	$stringData,
+	$integerData,
+	$floatData,
+	$boolData,
+	$arrayData,
+	$objectData,
+	$resourceData,
+	$nullData
+);
+
+echo "<pre>";
+var_dump($allData);
+echo "<pre>";
 

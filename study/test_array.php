027325fadedbc795a4be8b27be5a8feb591c031c
r-nakaizumi <r-nakaizumi@crm-coltd.com>
2018-09-14 13:13:56 +0900

    test_now

--- study/test_array.php
+++ study/test_array.php
@@ -3,15 +3,25 @@
 
 $foods = array("そば","ラーメン","うどん");
 
-echo $foods[0]."<br/>";
-echo $foods[1]."<br/>";
-echo $foods[2]."<br/>";
+//echo $foods[0]."<br/>";
+//echo $foods[1]."<br/>";
+//echo $foods[2]."<br/>";
 
 $fruits[] = "リンゴ";
 $fruits[] = "バナナ";
 $fruits[] = "オレンジ";
 
-echo $fruits[0]."<br/>";
-echo $fruits[1]."<br/>";
-echo $fruits[2]."<br/>";
+//echo $fruits[0]."<br/>";
+//echo $fruits[1]."<br/>";
+//echo $fruits[2]."<br/>";
+
+$var1 = "文字列";
+$var2 = 100;
+$var3 = TRUE;
+$array = array("日本","アメリカ","イギリス");
+
+var_dump($var1); echo "<br/>";
+var_dump($var2); echo "<br/>";
+var_dump($var3); echo "<br/>";
+var_dump($array); echo "<br/>";
 
